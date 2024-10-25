a83303335c04060093e1ab7ecdc7cf5371d087d6
Eldorjon <macbookpro@Macbooks-MacBook-Pro.local>
2024-10-25 20:04:27 +0500

    fixed

--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -2,7 +2,6 @@ import Breadcrumps from "@/components/breadcrump";
 import Content from "@/components/content";
 import CourseHeader from "@/components/courses-header";
 import Header from "@/components/header/header";
-
 const Courses = () => {
 	return (
 		<>
